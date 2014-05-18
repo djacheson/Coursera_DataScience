d48d72a87c2af68934d0cdc71621df93d30ad6d8
djacheson <djacheson@gmail.com>
2014-05-18 23:02:25 +0200

    helloworld

---
+++ Getting_and_Cleaning_Data/run_Analysis.R
@@ -0,0 +1,63 @@
+run_Analysis <- function() {
+    require(stringr)
+    setwd("D:\\GitHub\\Coursera_DataScience\\Getting_and_Cleaning_Data\\UCI HAR Dataset")
+    
+    #Read Features
+    features <- read.table(file="features.txt",col.names=c("featureNumber","featureName"))
+    
+    #Read Activity Labels
+    labels <- read.table(file="activity_labels.txt",col.names=c("Activity_Number","Activity_Label"))
+    
+    #Read train, and then extract columns with mean and std in the name
+    X_train <- read.table(file=".\\train\\X_train.txt",header=F)
+    #This isn't working because first columns is a space :(
+    #X_train <- fread(input=".\\train\\X_train.txt",header=F, sep=" ",skip>=1)
+    
+    X_test <- read.table(file=".\\test\\X_test.txt",header=F)
+    names(X_train) <- features$featureName
+    names(X_test) <- features$featureName
+    
+    #mean and std booleans
+    meanBool <- str_detect(features$featureName,"mean()")
+    stdBool <- str_detect(features$featureName,"std()")
+    mean_stdBool <- meanBool | stdBool
+    
+    X_train <- X_train[mean_stdBool]
+    X_test <- X_test[mean_stdBool]
+    
+    #Read Y_data, which is the activity
+    #re-label the numbers as the activity 
+    
+    Y_train <- read.table(file=".\\train\\y_train.txt",header=F)
+    Y_test <- read.table(file=".\\test\\y_test.txt",header=F)
+    names(Y_train) <- "Activity_Number"
+    names(Y_test) <- "Activity_Number"
+    
+    Y_train <- merge(Y_train, labels)
+    Y_test <- merge(Y_test, labels)
+    
+    #Read subject data
+    subject_train <- read.table(file="./train/subject_train.txt")
+    names(subject_train) <- "subject"
+    subject_test <- read.table(file="./test/subject_test.txt")
+    names(subject_test) <- "subject"
+
+    
+    #Start combining the data
+    train_All <- cbind(subject_train,X_train,Y_train)
+    test_All <- cbind(subject_test,X_test,Y_test)
+    
+    data_Combined <- rbind(train_All,test_All)
+    
+    return(data_Combined)
+    #Consider removing unnecessary variables here
+    #rm(list=c())
+}
+
+makeTidy <- function(data) {
+  #Function to create a tidy data set using data.table package
+  require(data.table)
+  data <- data.table(data)
+  tidy <- data[, lapply(.SD,mean), by="subject,Activity_Label"]
+  return(tidy)
+}
